3d4e665292ce499e6069485e22d86ec00b7fea7e
Nathan <nsrirama@smu.edu>
2021-10-29 16:01:12 -0500

    Initial Commit

---
+++ MSL-Lab-Four/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSL-Lab-Four
+//
+//  Created by UbiComp on 10/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MSL-Lab-FourUITests/MSL_Lab_FourUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MSL_Lab_FourUITestsLaunchTests.swift
+//  MSL-Lab-FourUITests
+//
+//  Created by UbiComp on 10/29/21.
+//
+
+import XCTest
+
+class MSL_Lab_FourUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
